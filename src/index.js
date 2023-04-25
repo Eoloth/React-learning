eba641c61d32f73e76dc8b54a08a25b7e7956764
Eoloth <53416886+Eoloth@users.noreply.github.com>
2023-04-25 14:57:16 -0400

    Const, let, var

--- src/index.js
+++ src/index.js
@@ -1,26 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const myFirstElement = (
-    <table>
-        <tr>
-            <th>Name</th>
-        </tr>
-        <tr>
-            <td>John</td>
-        </tr>
-        <tr>
-            Elsa
-        </tr>
-    </table>
-);
+const myelement = ("Hello")
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(myFirstElement);
+root.render(myelement);
 
 
 // If you want to start measuring performance in your app, pass a function
