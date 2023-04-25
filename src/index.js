f9d1068903c4e295df360e444b98c7919ab9d1a0
Eoloth <53416886+Eoloth@users.noreply.github.com>
2023-04-25 13:57:42 -0400

    First React push

--- src/index.js
+++ src/index.js
@@ -4,12 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const myFirstElement = (
+    <table>
+        <tr>
+            <th>Name</th>
+        </tr>
+        <tr>
+            <td>John</td>
+        </tr>
+        <tr>
+            Elsa
+        </tr>
+    </table>
 );
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(myFirstElement);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
